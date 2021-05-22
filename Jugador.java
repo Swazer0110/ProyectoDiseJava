6edf42d27e43b30a56544800409da7919c8da5a3
Swazer0110 <75527943+Swazer0110@users.noreply.github.com>
2021-05-22 21:21:04 +0200

    Clase Jugador.java

---
+++ Jugador.java
@@ -0,0 +1,71 @@
+/**
+* Jugador
+*/
+import java.util.*; 
+public class Jugador {
+    private String arma;
+    private int fuerza,resistencia,magia;
+    Jugador(){
+        setArma(eleccion());
+        switch(this.arma){
+        case "Hacha":
+            setFuerza(70);
+            setResistencia(50);
+            setMagia(30);
+            break;
+        case "Espada":
+            setFuerza(40);
+            setResistencia(70);
+            setMagia(40);
+            break;
+        case "Baston":
+            setFuerza(30);
+            setResistencia(40);
+            setMagia(80);
+            break;
+        }
+    }
+    public String getArma() {
+        return arma;
+    }
+    public int getFuerza() {
+        return fuerza;
+    }
+    public int getMagia() {
+        return magia;
+    }
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+    public void setMagia(int magia) {
+        this.magia = magia;
+    }
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+    public String eleccion(){
+        Scanner sc= new Scanner(System.in);
+        String arma=new String();
+        System.out.println("Elige tu arma:\n1-Hacha\n2-Espada\n3-Baston");
+        int i= sc.nextInt();
+        switch(i) {
+            case 1:
+                arma="Hacha";
+                break;
+            case 2:
+                arma="Espada";
+                break;
+            case 3:
+                arma="Baston";
+                break;
+            default:
+                arma="Espada";
+                break;
+        }
+        sc.close();
+        return arma;
+    }
+}
